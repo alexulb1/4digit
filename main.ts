dbd4dc34acd7ce2b0b979385b07a4d3e86df7568
alexulb1 <109351274+alexulb1@users.noreply.github.com>
2026-03-26 15:07:43 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,9 @@
+let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
+_4digit.set(4)
+basic.forever(function () {
+    for (let Index = 0; Index <= 20; Index++) {
+        _4digit.show(Index)
+        basic.pause(500)
+        _4digit.clear()
+    }
+})
