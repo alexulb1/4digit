7773cfddac818ca40223f7f010b903eb724cbdbf
alexulb1 <109351274+alexulb1@users.noreply.github.com>
2026-03-26 15:09:42 +0100

    Rückwärts zählen

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
 _4digit.set(4)
 basic.forever(function () {
     for (let Index = 0; Index <= 20; Index++) {
-        _4digit.show(Index)
+        _4digit.show(20 - Index)
         basic.pause(500)
         _4digit.clear()
     }
